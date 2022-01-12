0138f060211af71f00fcf9586d95f235921a786b
Novopashin Vladimir <vnovopashin@vtb.ru>
2022-01-12 14:22:56 +0300

    Make function

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import itertools as it
+def printSum(b, array, times):
+    comb = it.combinations(array, times)  # it.combinations если не хотите учитывать обратные перестановки
+    result = [i for i in comb if sum(i) == b]
+    print(result)
 
-
-a = [2,4,5,6,9]
-b = 18
-comb = it.combinations(a, 3) # it.combinations если не хотите учитывать обратные перестановки
-print(comb)
-
-result = [i for i in comb if sum(i) == b]
-
-print(result)
+a = [1,2,3,4,5,6,7,8,9]
+b = 12
+printSum(b, a, 3)
